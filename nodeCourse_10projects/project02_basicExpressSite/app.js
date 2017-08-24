572ac7aa41451eef1c87b97469dbbcdea4b6f213
Rafael Meneguzzo Sartor <rafaelmsartor@gmail.com>
2017-08-24 20:50:34 -0300

    added index and layout that was caught from getbootstrap.com server rendering jade views how to pass a variable to a jade view how to add views to the server

--- nodeCourse_10projects/project02_basicExpressSite/app.js
+++ nodeCourse_10projects/project02_basicExpressSite/app.js
@@ -5,13 +5,23 @@ var bodyParser = require( "body-parser" );
 var nodemailer = require( "nodemailer" );
 
 var app = express();
+
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'jade' );
+
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( {extended: false } ) );
+
+app.use( express.static( path.join( __dirname, 'public' ) ) );
 
 const port = 3000
 
 app.get( '/', (req, res) => {
-    res.send( "Hello World!" );
+    res.render( 'index', {title: "Welcome"} );
+});
+
+app.get( '/about', (req, res) => {
+    res.render( 'about' );
 });
 
 app.listen( port );
